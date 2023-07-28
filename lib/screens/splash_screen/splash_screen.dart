7fbe4db7ce26b8046826a7fea8e1a0ff5d30639b
Maleesha Herath <malishaherath77@gmail.com>
2023-07-28 19:36:33 +0530

    a

--- lib/screens/splash_screen/splash_screen.dart
+++ lib/screens/splash_screen/splash_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 import 'package:kwik_mart/components/custom_poppins_text.dart';
 import 'package:kwik_mart/screens/auth/singnin_page.dart';
 
@@ -11,17 +12,17 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
-  // @override
-  // void initState() {
-  //   super.initState();
-  //   Future.delayed(const Duration(seconds: 5), () {
-  //     Navigator.push(
-  //         context,
-  //         MaterialPageRoute(
-  //           builder: (context) => const SingInPage(),
-  //         ));
-  //   });
-  // }
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(seconds: 6), () {
+      Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => const SingInPage(),
+          ));
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -34,13 +35,29 @@ class _SplashScreenState extends State<SplashScreen> {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Image(
-                  width: size.width * 0.3,
-                  image: const AssetImage('assets/images/splashLogo.png'),
+                Animate(
+                  effects: [
+                    FadeEffect(duration: 500.ms),
+                    ScaleEffect(),
+                  ],
+                  child: Image(
+                    width: size.width * 0.3,
+                    image: const AssetImage('assets/images/splashLogo.png'),
+                  ),
                 ),
-                const Padding(
-                  padding: EdgeInsets.only(top: 15, bottom: 20),
-                  child: CustomPoppinsText(text: "Kwik Mart"),
+                Animate(
+                  effects: [
+                    FadeEffect(duration: 500.ms),
+                  ],
+                  child: Padding(
+                    padding: EdgeInsets.only(top: 15, bottom: 20),
+                    child: CustomPoppinsText(
+                      text: "Kwik Mart",
+                      color: Colors.grey.shade500,
+                      fontSize: 32,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
                 ),
                 const CupertinoActivityIndicator(),
               ],
