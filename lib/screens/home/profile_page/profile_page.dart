0b451ff9e4457bea803cc9d290b128ffa6703bc7
Maleesha Herath <malishaherath77@gmail.com>
2023-07-29 17:36:07 +0530

    ds

--- lib/screens/home/profile_page/profile_page.dart
+++ lib/screens/home/profile_page/profile_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:kwik_mart/components/custom_poppins_text.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
@@ -10,8 +11,23 @@ class ProfilePage extends StatefulWidget {
 class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Profile Page"),
+    return SafeArea(
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Image.network(
+              "https://static-01.daraz.lk/p/825a0d14604528f01b1ff003dc32f33c.jpg",
+              width: 150,
+            ),
+            CustomPoppinsText(
+              text: "Name",
+              fontSize: 16,
+            )
+          ],
+        ),
+      ),
     );
   }
 }
